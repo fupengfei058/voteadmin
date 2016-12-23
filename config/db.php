7f69c0c5003c4a39e734bdbbfb05d4c4f73b102e
fupengfei <fupengfei@yaochufa.com>
2016-12-23 17:04:17 +0800

    12.23

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.235.128;dbname=vote',
+    'dsn' => 'mysql:host=192.168.235.129;dbname=vote',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
